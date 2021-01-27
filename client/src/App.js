9500207ef272f96b15d573e2832a3e20bfb0df94
Orlando Castillo <omaddoc@gmail.com>
2021-01-27 18:02:01 -0500

    MVP

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Route, Switch } from "react-router-dom";
+import SavedList from "./Movies/SavedList";
 
-import SavedList from './Movies/SavedList';
+import Movie from "./Movies/Movie";
+// import MovieCard from "./Movies/MovieCard";
+import MovieList from "./Movies/MovieList";
 
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .get("http://localhost:5000/api/movies") // Study this endpoint with Postman
+        .then((response) => {
           // Study this response with a breakpoint or log statements
+          // console.log(response.data);
           // and set the response data as the 'movieList' slice of state
+          setMovieList(response.data);
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
+    };
     getMovies();
   }, []);
-
-  const addToSavedList = id => {
+  // console.log(movieList);
+  const addToSavedList = (id) => {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
+      <SavedList
+        list={
+          [
+            /* This is stretch */
+          ]
+        }
+      />
 
-      <div>Replace this Div with your Routes</div>
+      {/* <div>Replace this Div with your Routes</div> */}
+      <Switch>
+        <Route path="/movies/:id">
+          <Movie />
+        </Route>
+        <Route path="/">
+          <MovieList movies={movieList} />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,13 @@
-import React from 'react';
+import React from "react";
+import MovieDetails from "./MovieDetails";
 
 export default function MovieList(props) {
+  const { movies } = props;
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {movies.map((movie) => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
-    </div>
-  );
-}
-
-function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
-
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
     </div>
   );
 }
